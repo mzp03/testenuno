173b1ca1596879f49bacdef542b3ed7b14748da9
mzp03 <mzp03069@disaq.com>
2018-11-01 11:56:02 +0000

    http

--- wp-login.php
+++ wp-login.php
@@ -9,19 +9,19 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Create a Website &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='https://learnandbe.eu/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' href='http://learnandbe.eu/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="https://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-32x32.png" sizes="32x32" />
-<link rel="icon" href="https://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-192x192.png" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="https://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-180x180.png" />
-<meta name="msapplication-TileImage" content="https://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-270x270.png" />
+	<link rel="icon" href="http://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-32x32.png" sizes="32x32" />
+<link rel="icon" href="http://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="http://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-180x180.png" />
+<meta name="msapplication-TileImage" content="http://learnandbe.eu/wp-content/uploads/2018/10/cropped-img1-1-270x270.png" />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="https://learnandbe.eu/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="http://learnandbe.eu/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -33,13 +33,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="https://learnandbe.eu/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="http://learnandbe.eu/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="https://learnandbe.eu/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="http://learnandbe.eu/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -56,7 +56,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="https://learnandbe.eu/">&larr; Back to Create a Website</a></p>
+	<p id="backtoblog"><a href="http://learnandbe.eu/">&larr; Back to Create a Website</a></p>
 		
 	</div>
 
